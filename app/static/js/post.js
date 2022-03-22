12666d756349da441378a7823e71917e013563c3
qwergasgh <qwergasgh@gmail.com>
2022-03-22 22:22:44 +0300

    show/remove images post

--- app/static/js/post.js
+++ app/static/js/post.js
@@ -9,31 +9,38 @@ $.ajaxSetup({
 })
 
 function get_images(id) {
-    $.ajax({
-        type: 'POST',
-        url: '/blog/get_images',
-        data: JSON.stringify({id_post: id}),
-        contentType: 'application/json',
-        success: function(response) {
-            console.log('valid: ' + response.valid);
-            for (i in response.path_images) {
-                img = document.createElement('img');
-                img.class = "img-rounded profile-thumbnail";
-                // img.src = "{{ url_for('static', filename='" + response.path_images[i] + "') }}";
-                img.src = '/static/' + response.path_images[i];
-                img.width = "256";
-                img.height = "256";
-                document.getElementById(id).append(img);
+    id_element = id.toString();
+    if (document.getElementById(id_element + "_button").textContent == "Remove Images") {
+        document.getElementById(id_element + "_button").textContent = "Show Images";
+        document.getElementById(id_element + '_div').remove();
+    } else {
+        $.ajax({
+            type: 'POST',
+            url: '/blog/get_images',
+            data: JSON.stringify({id_post: id}),
+            contentType: 'application/json',
+            success: function(response) {
+                console.log('valid: ' + response.valid);
+                if (response.path_images.length > 0) {
+                    div = document.createElement('div');
+                    div.id = id_element + '_div';
+                    document.getElementById(id_element).append(div)
+                    console.log(response.path_images.length, div.id)
+                    for (i in response.path_images) {
+                        img = document.createElement('img');
+                        img.class = "img-rounded profile-thumbnail";
+                        // img.src = "{{ url_for('static', filename='" + response.path_images[i] + "') }}";
+                        img.src = '/static/' + response.path_images[i];
+                        img.width = "256";
+                        img.height = "256";
+                        document.getElementById(div.id).append(img);
+                    }
+                    document.getElementById(id_element + "_button").textContent = "Remove Images";
+                }
+            },
+            error: function(error) {
+                console.log(error.status + ' ' + error.valid);
             }
-            document.getElementById(id.toString() + "_button").onclick = "disable_images(" + id + ")";
-            document.getElementById(id.toString() + "_button").textContent = "Remove Images";
-        },
-        error: function(error) {
-            console.log(error.status + ' ' + error.valid);
-        }
-    });
-}
-
-function disable_images(id) {
-    console.log('ok')
+        });
+    }
 }
